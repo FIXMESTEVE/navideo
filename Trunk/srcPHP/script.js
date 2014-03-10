c1bc3fff3c1d7ef79dd97ba89140c698ef0afa32
xaveir <x.jouvenot@gmail.com>
2014-03-10 12:50:46 +0100

    Modif cacher menu
    J'avais modifier des id qui empecher de cahcer le menu xp
    C'est corrigé maintenant

--- Trunk/srcPHP/script.js
+++ Trunk/srcPHP/script.js
@@ -1,14 +1,11 @@
 function hideMenu(button){
-	var stateMenuGuest = document.getElementById("menuGuest").style.display;
-	//var stateMenuConnexion = document.getElementById("connexion").style.display;
-	if(stateMenuGuest == "none"){
-		document.getElementById("menuGuest").style.display = "block";
-		//document.getElementById("connexion").style.display = "block";
+	var stateMenu = document.getElementsByTagName("HEADER")[0].style.display;
+	if(stateMenu == "none"){
+		document.getElementsByTagName("HEADER")[0].style.display = "block";
 		button.textContent = "Cacher les menus";
 	}
 	else{
-		document.getElementById("menuGuest").style.display = "none";
-		//document.getElementById("connexion").style.display = "none";
+		document.getElementsByTagName("HEADER")[0].style.display = "none";
 		button.textContent = "Afficher les menus";
 	}
 }
