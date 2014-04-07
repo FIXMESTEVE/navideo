2c011a61e7952c7e329b30a53382029e6f168e85
xaveir <x.jouvenot@gmail.com>
2014-04-07 23:38:08 +0200

    View  send XML

--- Trunk/srcPHP/View/Form/DoctorForm/SendXML.php
+++ Trunk/srcPHP/View/Form/DoctorForm/SendXML.php
@@ -3,9 +3,19 @@ include_once "srcPHP/View/Form/FormView.php";
 
 class SendXML extends FormView{
 
+	var $video = NULL;
+	var $model = NULL;
+
 	function SendXML($action){
 		try{
 			parent::FormView($action);
+			if(isset($_GET) && isset($_GET["form"]) && $_GET["form"] === "sendXML" && isset($_GET["video"])){
+				$this->video = $_GET["video"];
+				$this->model = new DoctorModel("dbserver", "xjouveno", "xjouveno", "pdp");
+				$this->execute();
+			}
+			else
+				throw new Exception("ERREUR - SendXML(...) - Verifier les parametres dans l'url");
 		} catch(Exception $e){
 			echo $e->getMessage();
 		}
@@ -17,11 +27,17 @@ class SendXML extends FormView{
 
 	function onLoadJS(){ }
 
-	function execute(){ }
+	function execute(){
+			if(isset($_GET) && isset($_GET["form"]) && $_GET["form"] === "sendXML" && isset($_GET["video"]) && isset($_GET["execute"]) && $_GET["execute"] === "true"){
+				if(isset($_POST) && isset($_POST["xml_file"]) && !empty($_POST["xml_file"])){
+
+				}
+			}
+	}
 
 	function draw(){
-		echo "<h1>En Cours</h1>";
-		echo "<form id=\"sendXML\" action=\"".$this->action."?form=sendXML&execute=true\" method=\"post\">";
+		echo "<form id=\"sendXML\" action=\"".$this->action."?form=sendXML&video=".$this->video."&execute=true\" method=\"post\">";
+		echo "<label>Fichier XML a ajouter :</label><input type=\"file\" name=\"xml_file\"/>";
 		echo "<input type=\"submit\" value=\"Envoyer\">";
 		echo "</form>";
 	}
