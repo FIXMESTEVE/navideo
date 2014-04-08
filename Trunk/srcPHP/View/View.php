b64c3b1745f91b34c4cc0c7597a310d0c850bac6
xaveir <x.jouvenot@gmail.com>
2014-04-08 13:35:37 +0200

    doxygen View.php

--- Trunk/srcPHP/View/View.php
+++ Trunk/srcPHP/View/View.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ *\file		View.php
+ *\author	Antoine Laumond, Romain Fontaine, Tom Solacroup, Xavier Jouvenot
+ *\version	2.0
+ *\date		08 Avril 2014
+ *\brief	Defini l'interface principale de la vue cette application.
+ *
+ *\details	Cette interface defini les fontions de tous les elements de la vue
+ *			draw pour l'afficher
+ *			linkCSS pour relier la ou les feuilles de style de l'objet
+ *			linkJS pour relier le ou les scripts nécessaire à l'objet
+ *			onLoadJS pour appeler les fonctions nécessaires, lors du chargement de la page
+ */
 interface View{
 	function draw();
 	function linkCSS();
