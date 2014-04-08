97cd91fc40c13580ed2fc91b7c1d61ffcb1f8d82
xaveir <x.jouvenot@gmail.com>
2014-04-08 11:47:33 +0200

    doxygen main.php

--- Trunk/srcPHP/main.php
+++ Trunk/srcPHP/main.php
@@ -1,14 +1,34 @@
 <?php
 
+/**
+ *\file		main.php
+ *\author	Antoine Laumond, Romain Fontaine, Tom Solacroup, Xavier Jouvenot
+ *\version	2.0
+ *\date		08 Avril 2014
+ *\brief	Defini la classe principale de cette application.
+ *
+ *\details	Cette classe permet de structurer la page web en plusieurs grandes parties distinctes:
+ *		le menu, le contenu de la page. Elle perm egalement d'instancier les objets de l'application en cascadede,
+ *		de lier les divers fichiers CSS et JS de ceux-ci et de les afficher.
+ */
+
+
 include_once "View/Menu/MenuView.php";
 include_once "View/Section/SectionView.php";
 include_once "View/Player/Tag/AddTag.php";
 
 class Main{
-	var $menu;
-	var $section;
-	var $addTagMenu;
+	/* Variables */
+	var $menu = NULL;
+	var $section = NULL;
+	var $addTagMenu = NULL;
 
+
+	/**
+	 *\brief	Contructeur de la classe Main
+	 *\details	Fontion permettant d'instancier les variables de la classe Main
+	 *
+	 */
 	function Main(){
 		try{
 			$this->menu = new MenuView();
@@ -20,6 +40,12 @@ class Main{
 
 	}
 
+	/**
+	 *\brief	Fonction d'affichage de la classe Main
+	 *\details	Cette fontion permet de mettre en place la structure d'une page web
+	 *		et de lier les divers CSS etS a la page, ainsi que d'affichier les elements
+	 *		qui en font partis.
+	 */
 	function run(){
 		echo "<!doctype html>";
 		echo "<html>";
@@ -28,8 +54,8 @@ class Main{
 		echo "<script type=\"text/javascript\" src=\"js/SM2/soundmanager2.js\"></script>";
 		echo "<script type=\"text/javascript\" src=\"js/utils.js\"></script>";
 		echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"css/body.css\">";
-	    echo "<!-- Bootstrap -->";
-	    echo "<link href=\"css/bootstrap.min.css\" rel=\"stylesheet\">";
+		echo "<!-- Bootstrap -->";
+		echo "<link href=\"css/bootstrap.min.css\" rel=\"stylesheet\">";
 		$this->menu->linkCSS();
 		$this->section->linkCSS();
 		$this->section->linkJS();
@@ -40,12 +66,12 @@ class Main{
 		$this->menu->draw();
 		$this->section->draw();
 		$this->addTagMenu->draw();
-		
+
 		echo "<button onClick=\"hideMenu(this);\">Cacher les menus</button>";
 		echo "<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->";
-	    echo "<script src=\"js/jquery.min.js\"></script>";
-	    echo "<!-- Include all compiled plugins (below), or include individual files as needed -->";
-	    echo "<script src=\"js/bootstrap.min.js\"></script>";
+		echo "<script src=\"js/jquery.min.js\"></script>";
+		echo "<!-- Include all compiled plugins (below), or include individual files as needed -->";
+		echo "<script src=\"js/bootstrap.min.js\"></script>";
 
 		echo "</body>";
 		echo "</html>";
