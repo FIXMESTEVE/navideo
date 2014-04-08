d837b27d309ca65f24a12f6a9b7b43f6512cbca6
xaveir <x.jouvenot@gmail.com>
2014-04-08 10:50:40 +0200

    doxygen indexx_light

--- Trunk/index_light.php
+++ Trunk/index_light.php
@@ -1,5 +1,20 @@
 <?php
+/**
+ *\file		index_light.php
+ *\author	Antoine Laumond, Romain Fontaine, Tom Solacroup, Xavier Jouvenot
+ *\version	2.0
+ *\date		08 Avril 2014
+ *\brief	Fichier permettant de n'avoir qu'un lecteur videoeger
+ *
+ *\details	Ce fichier n'integre que le lecteur video et la liste des observations
+ *		liee a celle-ci. La videee et le commencement de la lecture sont
+ *		fournie par l'url ($_GET).
+ */
+
+
 	include_once "srcPHP/View/Player/PlayerView.php";
+
+	/* instanciation du player */
 	$player = NULL;
 	if(isset($_GET) && isset($_GET["play"]) && isset($_GET["start"]))
 		$player = new PlayerView($_GET["play"], $_GET["start"]);
@@ -14,6 +29,7 @@
 <link rel="stylesheet" type="text/css" href="css/body.css">
 
 <?php
+	/* Liaison avec les fichiers CSS et JS du player */
 	if(isset($_GET) && isset($_GET["play"]) && isset($_GET["start"])){
 		$player->linkCSS();
 		$player->linkJS();
@@ -23,6 +39,7 @@
 </head>
 
 <?php
+	/* Affichage du player */
 	if(isset($_GET) && isset($_GET["play"]) && isset($_GET["start"])){
 		echo "<body onload=".$player->onLoadJS().">";
 		$player->draw();
