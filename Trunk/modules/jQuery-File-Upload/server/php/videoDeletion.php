52fa06fda6a5a379807b0b024ae7f94a8ec200f3
s4e-tom <tom.solacroup@gmail.com>
2014-04-08 06:08:23 +0200

    Bugfix sur la suppression de vignette

--- Trunk/modules/jQuery-File-Upload/server/php/videoDeletion.php
+++ Trunk/modules/jQuery-File-Upload/server/php/videoDeletion.php
@@ -3,10 +3,13 @@
 	
 	//cleans the database of the erased file
 	$result = pg_query($db, "DELETE FROM \"Video\" WHERE filename='$_POST[filename]' RETURNING \"idVideo\";") or die(pg_last_error());
-	$insert_row = pg_fetch_row($result);
-	$insert_id = $insert_row[0];
+	$delete_row = pg_fetch_row($result);
+	$delete_id = $delete_row[0];
 
 	//erase its thumbnail
-	$cmd = "rm ".escapeshellarg('files/video_thumbnails/'.$insert_id.'.jpg');
-
+	$cmd = "rm ".escapeshellarg('files/video_thumbnails/'.$delete_id.'.jpg');
+	exec($cmd, $output, $code);
+	if ($code != 0) {
+	    throw new Exception("rm failed. Check the apache error log.");
+	}
 ?>
