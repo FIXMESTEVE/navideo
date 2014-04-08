0c81f516e141eafbb0273f14c6df9f9b909e9758
xaveir <x.jouvenot@gmail.com>
2014-04-08 13:44:15 +0200

    doxygen FormView.php

--- Trunk/srcPHP/View/Form/FormView.php
+++ Trunk/srcPHP/View/Form/FormView.php
@@ -1,10 +1,26 @@
 <?php
+/**
+ *\file		FormView.php
+ *\author	Antoine Laumond, Romain Fontaine, Tom Solacroup, Xavier Jouvenot
+ *\version	2.0
+ *\date		08 Avril 2014
+ *\brief	Defini la classe mère abstraite de vue des formulaires.
+ *
+ *\details	Cette classe permet de definir les fonctions nécessaire au bons fonctionnement des
+ *			différents formulaire des l'application
+ *			Les fonction définies par l'interface + une fonction execute
+ */
 include_once "srcPHP/View/View.php";
 
 abstract class FormView implements View{
-
+	/* Variables */
 	var $action = "";
 
+	/**
+	 *\brief	Contructeur de la classe FormView
+	 *\details	Fontion permettant d'instancier les variables de la classe Main
+	 *\param	$action		fichier qui va recevra le formulaire
+	 */
 	function FormView($action){
 		if(is_string($action))
 			$this->action = $action;
@@ -12,6 +28,9 @@ abstract class FormView implements View{
 			throw new Exception("ERREUR - FormView(...) - Verifier les types des parametres");
 	}
 
+	/**
+	 *\brief	Fonction abstraite qui servira a l'execution des formulaires Fille
+	 */
 	abstract function execute();
 
 	abstract function linkCSS();
