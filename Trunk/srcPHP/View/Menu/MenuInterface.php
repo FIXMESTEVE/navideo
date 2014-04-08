6a383beb11feece0f81bccc0ffe9c709fbef2eda
xaveir <x.jouvenot@gmail.com>
2014-04-08 14:41:43 +0200

    doxygen MenuInterface.php

--- Trunk/srcPHP/View/Menu/MenuInterface.php
+++ Trunk/srcPHP/View/Menu/MenuInterface.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ *\file		MenuInterface.php
+ *\author	Antoine Laumond, Romain Fontaine, Tom Solacroup, Xavier Jouvenot
+ *\version	2.0
+ *\date		08 Avril 2014
+ *\brief	Defini l'interface principale de la vue des menus de cette application.
+ *
+ *\details	Cette interface defini les fontions de tous les elements de la vue des menus
+ *			les fonctions de l'interface View
+ *			isLogged permet de savoir si l'utilisateur est loggé
+ *			disconnect permet à l'utilisateur de se déconnecter
+ */
 include_once "srcPHP/View/View.php";
 
 interface MenuInterface extends View{
